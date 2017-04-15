e2eb68c7964a79755e2c34ce2944a627e20e0e77
applewct <applewct@gmail.com>
2017-04-14 19:43:46 -0700

    Jackie update scheme

--- mongodb.js
+++ mongodb.js
@@ -6,15 +6,21 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
 	// we're connected!
+
 	var Schema = mongoose.Schema;
 
-	var personSchema = new Schema({
-		firstname: String,
+	var werkerSchema = new Schema({
+        firstname: String,
 		lastname: String,
-		address: String
+		licencenum: String,
+        phone: String,
+        review: String,
+        rating: { type: Number, min: 1, max: 5 },
+        skill:String
+
 	});
 
-	var Person = mongoose.model('Person', personSchema);
+	var Worker = mongoose.model('Worker', werkerSchema);
 
 	// var john = Person({
 	//   firstname: 'John',
@@ -42,11 +48,11 @@ db.once('open', function() {
 	//   console.log('person saved!');
 	// });
 
-	// get all the users
-	Person.find(function(err, users) {
+	// get all the worker
+    Worker.find(function(err, workers) {
 		if (err) throw err;
 
 		// object of all the users
-		console.log(users);
+		console.log(workers);
 	});
 });

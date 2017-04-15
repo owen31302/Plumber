6504a78f0201201531128c4d7b209053964f2e98
applewct <applewct@gmail.com>
2017-04-15 09:10:53 -0700

    Jackie debugging api

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -6,18 +6,18 @@ var Work = require('../models/workModel');
 
 module.exports = function (app) {
 
-    app.get('/api/', function (req, res) {
-
-        Worker.find({}, function(err, worker) {
-            if (err) throw err;
-            res.send(worker);
-        });
-
-        /*Worker.find({ "firstname": "John" }, function(err, worker) {
-            if (err) throw err;
-            res.send(worker);
-        });*/
-    });
+    // app.get('/api/', function (req, res) {
+    //
+    //     Worker.find({}, function(err, worker) {
+    //         if (err) throw err;
+    //         res.send(worker);
+    //     });
+    //
+    //     /*Worker.find({ "firstname": "John" }, function(err, worker) {
+    //         if (err) throw err;
+    //         res.send(worker);
+    //     });*/
+    // });
 
     app.get('/api/works/', function (req, res) {
         Work.find({}, 'work', function (err, docs) {
@@ -26,18 +26,16 @@ module.exports = function (app) {
     });
 
     app.get('/api/work/:problem/:zipcode', function (req, res) {
-        console.log(req.params.problem);
-        // console.log(req.params.problem["work"]);
+        // console.log(req.params.problem);
         // console.log(req.params.zipcode);
 
-        // Work.findOne({ 'work': req.params.problem.work }, function (err, docs) {
-        //     console.log(docs);
-        // });
+        Work.findOne({ 'work': req.params.problem }, function (err, docs) {
+            // console.log(docs.time);
+        });
 
         var result = {
-            "theselection" : "123",
-            "cost" : "500",
-            "time" : "1"
+            "cost" : docs.cost,
+            "time" : docs.time
         }
         res.send(result);
     });

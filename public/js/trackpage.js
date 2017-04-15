45bc4faaa09dac2ae138e234eab277ae2bd8576f
TzuPingChen <TpChen82427@gmail.com>
2017-04-15 07:55:11 -0700

    select done

--- public/js/trackpage.js
+++ public/js/trackpage.js
@@ -7,7 +7,15 @@ $(document).ready(function(){
         for(i = 0; i < 3; i++){
             $.each(data,function (key,value) {
                 console.log('yesyesyes');
-                document.getElementById(key).innerHTML= value;
+                if(key == "lat"){
+                    var lat = value;
+                }
+                else if(key == "log"){
+                    var log = value;
+                }
+                else{
+                    document.getElementById(key).innerHTML= value;
+                }
             });
         }
 

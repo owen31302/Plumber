36208fd522b089c3fabdcf38ed55e41d5525db03
applewct <applewct@gmail.com>
2017-04-14 23:37:45 -0700

    Jackie add worker model

--- controllers/setupController.js
+++ controllers/setupController.js
@@ -2,6 +2,7 @@
  * Created by owen on 4/14/17.
  */
 var Workers = require('../models/workerModel');
+var Works = require('../models/workModel');
 
 module.exports = function (app) {
 
@@ -9,6 +10,14 @@ module.exports = function (app) {
         // seed database
         var starterWorkers = require('../fakeworkers');
         Workers.create(starterWorkers, function(err, results) {
+            res.send(results);
+        });
+    });
+
+    app.get('/api/setupWorks', function(req, res) {
+        // seed database
+        var starterWorks = require('../allworks');
+        Works.create(starterWorks, function(err, results) {
             res.send(results);
         });
     });
